5c5ef0079b9c8dd0ff173938ce7eac3fe320b81d
Kevin Harrington <mad.hephaestus@gmail.com>
2019-10-16 16:16:11 -0400

    Adding files

---
+++ src/main/java/BowlerStudioExtended/MyMain.java
@@ -0,0 +1,11 @@
+package BowlerStudioExtended;
+
+import com.neuronrobotics.bowlerstudio.BowlerStudio;
+
+public class MyMain {
+
+	public static void main(String[] args) throws Exception {
+		BowlerStudio.main(args);
+	}
+
+}
